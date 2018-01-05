0ffc75712d603ff776ab7c3e1d7f2cf6454b4490
David Xie <david30xie@gmail.com>
2018-01-05 17:04:50 +0000

    Add ngrx and material2

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+const MODULES = [
+  MatButtonModule,
+  MatCheckboxModule
+];
+
+@NgModule({
+  imports: MODULES,
+  exports: MODULES,
+})
+
+export class MaterialModule { }
